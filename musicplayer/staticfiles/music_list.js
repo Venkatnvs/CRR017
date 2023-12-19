3ec283b847cff9b55c490697ec606379fea0999e
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2023-12-19 16:26:29 +0530

    2EE

--- musicplayer/staticfiles/music_list.js
+++ musicplayer/staticfiles/music_list.js
@@ -3,36 +3,36 @@ let allMusic = [
       name: "Harley Bird - Home",
       artist: "Jordan Schor",
       img: "music-1",
-      src: "music-1"
+      src: "Beat It.mp3"
     },
     {
       name: "Ikson Anywhere – Ikson",
       artist: "Audio Library",
       img: "music-2",
-      src: "music-2"
+      src: "musicplayer/staticfiles/songs/Billie Jean.mp3"
     },
     {
       name: "Beauz & Jvna - Crazy",
       artist: "Beauz & Jvna",
       img: "music-3",
-      src: "music-3"
+      src: "musicplayer/staticfiles/songs/Cant HelpFalling In Love.mp3"
     },
     {
       name: "Hardwind - Want Me",
       artist: "Mike Archangelo",
       img: "music-4",
-      src: "music-4"
+      src: "musicplayer/staticfiles/songs/Jam.mp3"
     },
     {
       name: "Jim - Sun Goes Down",
       artist: "Jim Yosef x Roy",
       img: "music-5",
-      src: "music-5"
+      src: "musicplayer/staticfiles/songs/Numb.mp3"
     },
     {
       name: "Lost Sky - Vision NCS",
       artist: "NCS Release",
       img: "music-6",
-      src: "music-6"
+      src: "musicplayer/staticfiles/songs/Sweetchildomine.mp3"
     },
   ];

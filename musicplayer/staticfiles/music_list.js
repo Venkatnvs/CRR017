c88efdb34e80298f9f48541a435d43413da05c53
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2023-12-19 16:39:18 +0530

    ece

--- musicplayer/staticfiles/music_list.js
+++ musicplayer/staticfiles/music_list.js
@@ -1,38 +1,63 @@
 let allMusic = [
     {
-      name: "Harley Bird - Home",
+      name: "Beat It",
       artist: "Jordan Schor",
       img: "music-1",
       src: "musicplayer/staticfiles/songs/Beat It.mp3"
     },
     {
-      name: "Ikson Anywhere – Ikson",
+      name: "Billie Jean",
       artist: "Audio Library",
       img: "music-2",
       src: "musicplayer/staticfiles/songs/Billie Jean.mp3"
     },
     {
-      name: "Beauz & Jvna - Crazy",
+      name: "Cant HelpFalling In Love",
       artist: "Beauz & Jvna",
       img: "music-3",
       src: "musicplayer/staticfiles/songs/Cant HelpFalling In Love.mp3"
     },
     {
-      name: "Hardwind - Want Me",
+      name: "Jam",
       artist: "Mike Archangelo",
       img: "music-4",
       src: "musicplayer/staticfiles/songs/Jam.mp3"
     },
     {
-      name: "Jim - Sun Goes Down",
+      name: "Numb",
       artist: "Jim Yosef x Roy",
       img: "music-5",
       src: "musicplayer/staticfiles/songs/Numb.mp3"
     },
     {
-      name: "Lost Sky - Vision NCS",
+      name: "Sweetchildomine",
       artist: "NCS Release",
       img: "music-6",
       src: "musicplayer/staticfiles/songs/Sweetchildomine.mp3"
     },
+    {
+        name: "TheRumbling",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "musicplayer/staticfiles/songs/TheRumbling.mp3"
+      },
+      {
+        name: "Thriller",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "musicplayer/staticfiles/songs/Thriller.mp3"
+      },
+      {
+        name: "UntilI Found You",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "musicplayer/staticfiles/songs/UntilI Found You.mp3"
+      },
+      {
+        name: "WeWillRockYou",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "musicplayer/staticfiles/songs/WeWillRockYou.mp3"
+      },
+
   ];

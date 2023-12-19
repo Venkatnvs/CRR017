fd2b151543e31c1cf76a16c4e00bae1db2bb708a
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2023-12-19 17:20:18 +0530

    added images

--- musicplayer/staticfiles/music_list.js
+++ musicplayer/staticfiles/music_list.js
@@ -2,61 +2,37 @@ let allMusic = [
     {
       name: "Beat It",
       artist: "Jordan Schor",
-      img: "music-1",
-      src: "musicplayer/staticfiles/songs/Beat It.mp3"
+      img: "Beat It",
+      src: "Beat It"
     },
     {
       name: "Billie Jean",
       artist: "Audio Library",
-      img: "music-2",
-      src: "musicplayer/staticfiles/songs/Billie Jean.mp3"
+      img: "Billie Jean",
+      src: "Billie Jean"
     },
     {
       name: "Cant HelpFalling In Love",
       artist: "Beauz & Jvna",
-      img: "music-3",
-      src: "musicplayer/staticfiles/songs/Cant HelpFalling In Love.mp3"
+      img: "Cant HelpFalling In Love",
+      src: "Cant HelpFalling In Love"
     },
     {
       name: "Jam",
       artist: "Mike Archangelo",
-      img: "music-4",
-      src: "musicplayer/staticfiles/songs/Jam.mp3"
+      img: "Jam",
+      src: "Jam"
     },
     {
       name: "Numb",
       artist: "Jim Yosef x Roy",
-      img: "music-5",
-      src: "musicplayer/staticfiles/songs/Numb.mp3"
+      img: "Numb",
+      src: "Numb"
     },
     {
       name: "Sweetchildomine",
       artist: "NCS Release",
-      img: "music-6",
-      src: "musicplayer/staticfiles/songs/Sweetchildomine.mp3"
+      img: "Sweetchildomine",
+      src: "Sweetchildomine"
     },
-    {
-        name: "TheRumbling",
-        artist: "NCS Release",
-        img: "music-6",
-        src: "musicplayer/staticfiles/songs/TheRumbling.mp3"
-      },
-      {
-        name: "Thriller",
-        artist: "NCS Release",
-        img: "music-6",
-        src: "musicplayer/staticfiles/songs/Thriller.mp3"
-      },
-      {
-        name: "UntilI Found You",
-        artist: "NCS Release",
-        img: "music-6",
-        src: "musicplayer/staticfiles/songs/UntilI Found You.mp3"
-      },
-      {
-        name: "WeWillRockYou",
-        artist: "NCS Release",
-        img: "music-6",
-        src: "musicplayer/staticfiles/songs/WeWillRockYou.mp3"
-      },
   ];
